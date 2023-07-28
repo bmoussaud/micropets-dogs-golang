bcf6c0f5b5ca6a2c92719730c474515c6e41cbf3
Benoit Moussaud <benoit@moussaud.org>
2023-07-28 14:42:41 +0200

    use lsp

--- apply_workload.sh
+++ apply_workload.sh
@@ -1,3 +1,4 @@
 NS=micropets-dev
 #kubectl apply -f config/pets_config.yaml -f config/serviceclaims-aria.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image akseutap6registry.azurecr.io/dogs --namespace $NS --yes  --update-strategy merge
+SOURCE="--source-image akseutap6registry.azurecr.io/dogs"
+tanzu apps workload apply -f config/workload.yaml --live-update --local-path .  --namespace $NS --yes  --update-strategy merge
